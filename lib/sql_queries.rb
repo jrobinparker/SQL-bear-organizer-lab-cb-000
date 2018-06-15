fb5c03e44811ca5a33b55d23436db7a68ad5e141
Jeremy Parker <jrobinparker@gmail.com>
2018-06-15 22:17:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  return "SELECT COUNT(temperament) from BEARS WHERE temperament != ' "
+  return "SELECT COUNT(temperament) from BEARS WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
